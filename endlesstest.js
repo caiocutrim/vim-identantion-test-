6402b8920213fd5b2836de1f6c7e0c7eb6b605a5
Caio Cutrim <kaiocoutrim@gmail.com>
2016-02-09 19:06:24 -0300

    another example added

--- endlesstest.js
+++ endlesstest.js
@@ -2,3 +2,8 @@ function functionName(hello) {
 	console.log('hello, world!');
 	console.log('please, replace this shit with spaces');
 }
+
+
+function thisneedtoworknow(comeon) {
+	console.log(common);
+}
